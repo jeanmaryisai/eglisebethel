d1c68ce924463a33df9f97ddd64e9847e2382d8a
jeanmaryisai <jeanmaryisai@gmail.com>
2023-03-24 12:43:31 -0400

    checkpoint

--- eglisebethel/urls.py
+++ eglisebethel/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('',RedirectView.as_view(url='/home')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
